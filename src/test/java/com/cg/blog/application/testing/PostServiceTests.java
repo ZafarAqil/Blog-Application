5704a9b856a4527507062bcf22556d9d9860a677
Ashish Bavaliya <ashishbavaliya818@gmail.com>
2021-04-15 09:44:06 +0530

    postservicetests updated

--- src/test/java/com/cg/blog/application/testing/PostServiceTests.java
+++ src/test/java/com/cg/blog/application/testing/PostServiceTests.java
@@ -1,17 +1,20 @@
 package com.cg.blog.application.testing;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
 import com.cg.blog.application.entities.Blogger;
 import com.cg.blog.application.entities.Community;
 import com.cg.blog.application.entities.Post;
 import com.cg.blog.application.entities.PostType;
+import com.cg.blog.application.exceptions.CommunityNotFoundException;
 import com.cg.blog.application.repositories.IBloggerRepository;
 import com.cg.blog.application.repositories.ICommunityRepository;
 import com.cg.blog.application.repositories.IPostRepository;
@@ -23,23 +26,34 @@ public class PostServiceTests {
 	@Autowired
 	PostServiceImpl postService;
 
-	@MockBean
+	@Autowired
 	IPostRepository postRepo;
 
-	@MockBean
+	@Autowired
 	ICommunityRepository communityRepository;
 
-	@MockBean
+	@Autowired
 	IBloggerRepository bloggerRepository;
 
+	@Transactional
 	@Test
 	public void testAddPost() {
 		Community community = new Community();
 		community.setCommunityId(1);
-//		communityRepository.save(community);
+		community.setTitle("community");
+		community.setCommunityDescription("Description");
+		List<Post> posts = new ArrayList<Post>();
+		community.setPosts(posts);
+		community = communityRepository.saveAndFlush(community);
+
 		Blogger blogger = new Blogger();
 		blogger.setId(1);
-		bloggerRepository.save(blogger);
+		blogger.setName("name");
+		blogger.setPassword("aaaaaaaa");
+		blogger.setEmail("xyz@gmail.com");
+		blogger.setPosts(posts);
+		blogger = bloggerRepository.saveAndFlush(blogger);
+
 		Post post = new Post();
 		post.setPostId(1);
 		post.setTitle("SampleTitle");
@@ -51,13 +65,51 @@ public class PostServiceTests {
 		post.setFlair("OC");
 		post.setCreatedBy(blogger);
 		post.setCommunity(community);
-		Mockito.when(postRepo.save(post)).thenReturn(post);
+		post = postRepo.saveAndFlush(post);
+
 		assertThat(postService.addPost(1, 1, post)).isEqualTo(post);
 	}
 
-//	@Test
-//	public void testAdd() {
-//		assertEquals(1, 1);
-//	}
+	@Transactional
+	@Test
+	public void testFailAddPost() {
+		Community community = new Community();
+		community.setCommunityId(1);
+		community.setTitle("community");
+		community.setCommunityDescription("Description");
+		List<Post> posts = new ArrayList<Post>();
+		community.setPosts(posts);
+		community = communityRepository.saveAndFlush(community);
 
+		Blogger blogger = new Blogger();
+		blogger.setId(1);
+		blogger.setName("name");
+		blogger.setPassword("aaaaaaaa");
+		blogger.setEmail("xyz@gmail.com");
+		blogger.setPosts(posts);
+		blogger = bloggerRepository.saveAndFlush(blogger);
+
+		final Post post = new Post();
+		post.setPostId(1);
+		post.setTitle("SampleTitle");
+		post.setDescription("SomethingUseless");
+		post.setContent(PostType.TEXT);
+		post.setNotSafeForWork(false);
+		post.setSpoiler(false);
+		post.setOriginalContent(false);
+		post.setFlair("OC");
+		post.setCreatedBy(blogger);
+		post.setCommunity(community);
+		postRepo.saveAndFlush(post);
+
+		assertThrows(CommunityNotFoundException.class, () -> postService.addPost(99, 1, post));
+//        Boolean thrown = false;
+//        try {
+//            postService.addPost(3, 1, post);
+//        }
+//        catch(CommunityNotFoundException e) {
+//            thrown = true;
+//        }
+//        assertTrue(thrown);
+	}
 }
